04ea117f27cca7b1599d4b6ad44b66108086d926
juanmartinezgarcia <juanmartinezg2004@gmail.com>
2024-03-12 12:37:23 +0100

    ClaseCoche1

---
+++ src/Coche.java
@@ -0,0 +1,18 @@
+public class Coche {
+    //Variables
+
+    String nombre;
+    Reloj tiempodevuelta;
+
+
+    //Constructores
+
+    public Coche(String nombre, Reloj tiempodevuelta){
+        this.nombre = nombre;
+        this.tiempodevuelta = tiempodevuelta;
+    }
+
+   
+
+    
+}
